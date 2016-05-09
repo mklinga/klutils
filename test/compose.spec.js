a82f89cda5c3d8d03ca8020a54915a903ca22204
Markus Klinga <laite@gmx.com>
2016-05-09 11:15:57 +0200

    Add some actual tests

--- test/compose.spec.js
+++ test/compose.spec.js
@@ -1,8 +1,21 @@
 import { compose } from "../src/index.js"
 
 describe('compose', () => {
-  it('should be a function', () => {
-    compose.should.exist
+  it('Should be a function', () => {
+    compose.should.be.a.Function()
+  })
+
+  it('Should return a function', () => {
+    compose().should.be.a.Function()
+  })
+
+  it('Should compose given data through the functions', () => {
+    const _value = 1
+    const doubleValue = x => x * 2
+    const tripleValue = y => y * 3
+
+    const composedFunc = compose(doubleValue, tripleValue)
+    composedFunc(_value).should.be.exactly(6)
   })
 })
 
